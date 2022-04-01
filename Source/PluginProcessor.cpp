042c7f9a7b78779efc995bbe2ae6bcedc98fca46
Nicholas Homayouni <>
2022-03-31 21:08:08 -0400

    Implemented DSP soft-clipping algorithm

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,11 +1,3 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
@@ -37,7 +29,7 @@ DistortionAudioProcessor::~DistortionAudioProcessor()
 }
 
 //==============================================================================
-/* Implement the function to give us our parameter layout*/
+/* Implement this function to give us our parameter layout*/
 juce::AudioProcessorValueTreeState::ParameterLayout DistortionAudioProcessor::createParameters()
 {
     // A vector of all the ranged audio parameters
@@ -49,7 +41,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout DistortionAudioProcessor::cr
     auto mixParam = std::make_unique<juce::AudioParameterFloat>("MIX", "mix", 0.0f, 100, 0.0f);
     auto outputParam = std::make_unique<juce::AudioParameterFloat>("OUTPUT", "outputDB", -24.0f, 24.0f, 0.0f);
     
-    /* Use the push_back method since it's a vector
+    /* Use the push_back method since it's a vector.
     We have to allocate memory for this unique_pointer.
     AudioParameterFloat IS a type of RangedAudioParameter.
     Give it all its initialization arugments (see docs) */
@@ -190,27 +182,29 @@ void DistortionAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
+    
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    
+    // Creating an Audio Block.
+    juce::dsp::AudioBlock<float> block (buffer);
+    
+    for(int channel = 0; channel < block.getNumChannels(); ++channel)
     {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
+        auto* channelData = block.getChannelPointer(channel);
+        
+        for(int sample = 0; sample < block.getNumSamples(); ++sample)
+        {
+            const auto input = channelData[sample] * juce::Decibels::decibelsToGain(inputDB);
+            
+            const auto softClip = piDivisor * std::atanf(input * juce::Decibels::decibelsToGain(driveDB));
+            
+            auto blend = input * (1.0 - mix) + softClip * mix;
+            
+            blend *= juce::Decibels::decibelsToGain(outputDB);
+            
+            channelData[sample] = blend;
+        }
     }
 }
 
